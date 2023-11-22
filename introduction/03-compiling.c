b871acb289397be5ea9a8068f81c355980cb5579
Svene <aidrecabrera@gmail.com>
2023-11-23 07:03:23 +0800

    lesson 3

---
+++ introduction/03-compiling.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  // output a line
+  printf("Hello World!\n");
+}
+// The way we can run a C program is by compiling it.
+// To compile a C program, we use the command gcc.
+// Example, to compile this file, we would run: gcc 03-compiling.c -o 03-compiling-compiled
+// The -o flag is used to specify the name of the compiled file.
+// The compiled file is the executable file that we can run.
