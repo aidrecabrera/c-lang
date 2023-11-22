d337c14e91fd74f61d63beb89494b5ceb5ce6ed7
Svene <aidrecabrera@gmail.com>
2023-11-23 07:00:12 +0800

    lesson 3

---
+++ introduction/02-comments.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  // This is a comment in C.
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+  /* There is a multi-line comment in C. */ 
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
